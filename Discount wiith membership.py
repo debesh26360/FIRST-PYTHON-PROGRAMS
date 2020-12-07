631fd32f1d85d6987e2255a2859c4f34116aacf5
Debesh Pradhan <51988668+debesh26360@users.noreply.github.com>
2020-12-07 22:52:17 +0530

    Update Discount wiith membership.py

--- Discount wiith membership.py
+++ Discount wiith membership.py
@@ -2,33 +2,33 @@ membership=input('type yes or no for membership ')
 
 if membership in ['y','Y','yes','YES']:
     
-    total_cost = int(input('Please enter a total_cost:'))
-    if total_cost>=1000:    
+    cost = int(input('Please enter a cost:'))
+    if cost>=1000:    
 
         print('You have won a discount by 15 percent')
-        total_cost *= 0.85
+        cost *= 0.85
     
-    elif total_cost<1000:
+    elif cost<1000:
 
         print("You have won a discount by 5 percent")
-        total_cost *= 0.95
+        cost *= 0.95
                
-    print ('Now the total cost is ', total_cost)
+    print ('Now the total cost is ', cost)
 
 elif membership in ['n','no','N','No']:
 
-    total_cost = int(input('Please enter a total_cost:'))
-    if total_cost>=1000:    
+    cost = int(input('Please enter a total_cost:'))
+    if cost>=1000:    
 
         print('You have won a discount by 5 percent')
-        total_cost *= 0.85
+        cost *= 0.85
     
-    elif total_cost<1000:
+    elif cost<1000:
 
         print("No discount")
-        total_cost *= 1
+        cost *= 1
                
-    print ('Now the total cost is ', total_cost)
+    print ('Now the total cost is ', cost)
 
 
 
